143c173389111ca71ca57039578e19a344983749
PlugAndCree <nicolamarangon123@gmail.com>
2021-09-13 09:52:14 +0200

    Added centralized lang file
    Every future plugin will use this centralized lang file for the most
    common messages

--- src/main/java/it/plugandcree/langutils/LangUtils.java
+++ src/main/java/it/plugandcree/langutils/LangUtils.java
@@ -1,12 +1,50 @@
 package it.plugandcree.langutils;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import it.plugandcree.langutils.config.CustomConfig;
 
 public class LangUtils extends JavaPlugin {
 	
+	private static LangUtils instance;
+	private CustomConfig centralizedLangConfig;
 	
 	@Override
 	public void onEnable() {
+		instance = this;
 		
+		setCentralizedLangConfig(createConfigFile("lang.yml"));
+	}
+	
+	private CustomConfig createConfigFile(String name) {
+		File fc = new File(getDataFolder(), name);
+		if (!fc.exists()) {
+			fc.getParentFile().mkdirs();
+			saveResource(name, false);
+		}
+
+		CustomConfig configFile = new CustomConfig();
+		try {
+			configFile.load(fc);
+			return configFile;
+		} catch (IOException | InvalidConfigurationException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public LangUtils getInstance() {
+		return instance;
+	}
+
+	public CustomConfig getCentralizedLangConfig() {
+		return centralizedLangConfig;
+	}
+
+	public void setCentralizedLangConfig(CustomConfig centralizedLangConfig) {
+		this.centralizedLangConfig = centralizedLangConfig;
 	}
 }
